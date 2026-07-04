dd99032390e7006640c87de0b3fea4585f429b31
k7-dev <testgitdevkali@tutamail.com>
2026-07-04 15:51:37 -0300

    atravessar rua

---
+++ codicao.py
@@ -0,0 +1,12 @@
+lado = "direita"
+
+print("estou olhando para a " + lado)
+
+if lado == "direita":
+    print("estou olhando pra esquerda")
+
+if lado == "esquerda":
+    print("estou olhando pra direita")
+
+
+print("atravesso a rua")
