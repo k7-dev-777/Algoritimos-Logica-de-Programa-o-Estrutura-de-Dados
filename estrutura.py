c0ff26ff3434493cced5fe214eae66f6fd5dae43
k7-dev <testgitdevkali@tutamail.com>
2026-07-04 19:13:49 +0000

    mexendo com objeto, bagulho dificil

---
+++ estrutura.py
@@ -0,0 +1,9 @@
+minhalista = [10, "Python", 3.14]
+print(minhalista) #[10, 'Python', 3.14]
+
+meudict = {"nome": "claudisnei", "idade": 25}
+print(meudict)# {'nome': 'claudisnei', 'idade': 25}
+
+meuset = {1, 2, 3, 3, 3, 4, 4}
+print(meuset)# {1, 2, 3, 4}
+
